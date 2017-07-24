67abb1e7e22ad3e876feaa6bd6e5be0968ae2ccd
Jessie Ding <nathalie.ding@gmail.com>
2017-07-24 17:00:03 +0800

    complete DefaultCustomerResponseTranslator

--- customer-api-boot/src/main/java/cn/com/easystudio/api/customer/service/DefaultCustomerResponseTranslator.java
+++ customer-api-boot/src/main/java/cn/com/easystudio/api/customer/service/DefaultCustomerResponseTranslator.java
@@ -2,22 +2,24 @@ package cn.com.easystudio.api.customer.service;
 
 import cn.com.easystudio.api.customer.entity.CustomerProfile;
 import cn.com.easystudio.api.customer.model.enumeration.Correctness;
+import cn.com.easystudio.api.customer.model.enumeration.CustomerLifecycle;
 import cn.com.easystudio.api.customer.model.enumeration.CustomerType;
-import cn.com.easystudio.api.customer.model.json.CustomerAttributes;
-import cn.com.easystudio.api.customer.model.json.CustomerData;
-import cn.com.easystudio.api.customer.model.json.CustomerLinks;
-import cn.com.easystudio.api.customer.model.json.VersionMeta;
+import cn.com.easystudio.api.customer.model.json.*;
 import cn.com.easystudio.api.customer.model.json.address.Address;
 import cn.com.easystudio.api.customer.model.json.address.Addresses;
 import cn.com.easystudio.api.customer.model.json.contact.ContactMethods;
 import cn.com.easystudio.api.customer.model.json.customer.ABN;
 import cn.com.easystudio.api.customer.model.json.customer.Audit;
+import cn.com.easystudio.api.customer.model.json.customer.Segment;
 import cn.com.easystudio.api.customer.model.json.customer.individual.Individual;
 import cn.com.easystudio.api.customer.model.json.customer.individual.IndividualName;
+import cn.com.easystudio.api.customer.model.json.customer.organisation.*;
 import org.joda.time.DateTime;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class DefaultCustomerResponseTranslator implements ResourceAssembler<CustomerProfile, Resource<CustomerData>> {
@@ -76,9 +78,36 @@ public class DefaultCustomerResponseTranslator implements ResourceAssembler<Cust
 
         //Individual 设值
         Individual individual = new Individual();
-        IndividualName name = new IndividualName("Chandler","Nelson","Green","SNR","MR","Chan","CHANDLER NELSON GREEN SNR",true,Correctness.CORRECT);
+        IndividualName name = new IndividualName("Chandler", "Nelson", "Green", "SNR", "MR", "Chan", "CHANDLER NELSON GREEN SNR",
+                true, Correctness.CORRECT);
         individual.setName(name);
 
+        //Organisation 设值
+        Organisation organisation = new Organisation();
+        ACN acn = new ACN("004085616", Correctness.CORRECT);
+        organisation.setAcn(acn);
+        ARBN arbn = new ARBN("123456789", Correctness.CORRECT);
+        organisation.setArbn(arbn);
+        CompanyType companyType = new CompanyType();
+        companyType.setCode("101601");
+        companyType.setDescription("a IT company");
+        organisation.setCompanyType(companyType);
+        OrganisationName organisationName = new OrganisationName("ROSTERING UNLIMITED PTY LTD", "ROSTERING UNLIMITED PTY !23123 LTD",
+                true, Correctness.CORRECT);
+        organisation.setName(organisationName);
+
+        //ProductSystemCustomerLinks 设值
+        List<ProductSystemCustomerLink> productSystemCustomerLinks = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            //插入测试数据
+            productSystemCustomerLinks.get(i).setBrand("PICC" + i);
+            productSystemCustomerLinks.get(i).setProductSystem("NPR" + i);
+            productSystemCustomerLinks.get(i).setProductSystemCustomerId("1700000015" + i);
+            productSystemCustomerLinks.get(i).setJointCustomer(true);
+        }
+
+        //Segment 设值
+        Segment segment = new Segment("12", "Young Establishers");
 
         customerAttributes.setAbn(abn);
         customerAttributes.setAddresses(addresses);
@@ -88,7 +117,11 @@ public class DefaultCustomerResponseTranslator implements ResourceAssembler<Cust
         customerAttributes.setContactMethods(contactMethods);
         customerAttributes.setCustomerType(CustomerType.ORGANISATION);
         customerAttributes.setIndividual(individual);
-       //TODO:set
+        customerAttributes.setLifecycle(CustomerLifecycle.CUSTOMER);
+        customerAttributes.setOrganisation(organisation);
+        customerAttributes.setProductSystemCustomerLinks(productSystemCustomerLinks);
+        customerAttributes.setSegment(segment);
+        customerAttributes.setWebsite("http://customer-live.com");
 
 
         //将Attributes设给CustomerData
